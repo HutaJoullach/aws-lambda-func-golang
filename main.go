a2f2860c352ed26e8b24803e17c5f2e5cae77963
HutaJoullach <hutajoullach@gmail.com>
2023-03-25 02:20:42 +0900

    changed message logic

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -16,7 +17,26 @@ type MyResponse struct {
 }
 
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+	cdate := time.Now()
+	cy, _, _ := cdate.Date()
+	yob := cy - event.Age
+
+	var gen string
+	if yob >= 1926 && yob <= 1945 {
+		gen = "Silent Generation"
+	} else if yob >= 1946 && yob <= 1965 {
+		gen = "Baby Boomers"
+	} else if yob >= 1966 && yob <= 1979 {
+		gen = "Generation X"
+	} else if yob >= 1980 && yob <= 1995 {
+		gen = "Millennials"
+	} else if yob >= 1996 {
+		gen = "Generation Z"
+	} else {
+		gen = "not in the group category"
+	}
+
+	return MyResponse{Message: fmt.Sprintf("Hey %s 👋 , and you are %s", event.Name, gen)}, nil
 }
 
 func main() {
